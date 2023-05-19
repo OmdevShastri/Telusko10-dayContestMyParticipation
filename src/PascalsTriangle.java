243843910134021caa7f18b923e3af4dc291e44a
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-05-20 01:28:11 +0530

    solving PascalsTriangle

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class PascalsTriangle {
+
     public static void main(String[] args) {
+        System.out.println("Give an n= ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int [][] arr;
+        //rows
+        for (int i = 0; i <= n; i++) {
+            //spaces
+            for (int j = 0; j <=n-i; j++) {
+                System.out.print(" ");
+            }
+            //nums just printing
+            for (int j=0;j<=i;j++){
+                System.out.print(fact(i)/(fact((i-j))*fact(j)) + " ");
+            }
+            System.out.println();
+        }
 
     }
+    public static int fact(int a){
+        if(a==1){
+            return 1;
+        }
+        return a * fact(a-1);
+    }
+
 }
+
