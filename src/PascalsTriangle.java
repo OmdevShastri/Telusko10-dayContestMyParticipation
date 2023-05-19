4ce0b4639aae9729bdf4dadd179ecb3bb6a0a6da
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-05-20 01:46:19 +0530

    progress in PascalsTriangle

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -7,27 +7,67 @@ public class PascalsTriangle {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int [][] arr;
-        //rows
-        for (int i = 0; i <= n; i++) {
-            //spaces
-            for (int j = 0; j <=n-i; j++) {
-                System.out.print(" ");
-            }
-            //nums just printing
-            for (int j=0;j<=i;j++){
-                System.out.print(fact(i)/(fact((i-j))*fact(j)) + " ");
-            }
-            System.out.println();
-        }
+        printPascRecursive(n);
+
 
     }
     public static int fact(int a){
-        if(a==1){
+        if(a==0){
             return 1;
         }
-        return a * fact(a-1);
+        return (a * fact(a-1));
     }
+
+    public static void printPascIterative(int n) {
+        //rows
+        for (int i = 0; i < n; i++) {
+
+            //spaces
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //nums printing iterative
+            for (int j = 0; j <= i; j++) {
+                System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void printPascRecursive(int n) {
+        //rows
+        for (int i = 0; i < n; i++) {
+
+            //spaces
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //nums printing iterative
+            for (int j = 0; j <= i; j++) {
+                System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printPascMemoization(int n) {
+        //rows
+        for (int i = 0; i < n; i++) {
+
+            //spaces
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //nums printing iterative
+            for (int j = 0; j <= i; j++) {
+                System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
 }
 
