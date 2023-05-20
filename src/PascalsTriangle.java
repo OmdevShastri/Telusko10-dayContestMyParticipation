4a4f732ebd9e3fcd58700e16375c6cb9a465b448
Omdev Shastri <omdevsh19ete@student.mes.ac.in>
2023-05-20 20:01:32 +0530

    done with PascalsTriangle

--- src/PascalsTriangle.java
+++ src/PascalsTriangle.java
@@ -1,13 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class PascalsTriangle {
+
+    private static Map<Integer, Integer> cache = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Give an n= ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        printPascIterative(n);
+        printPasc(n);
         printPascRecursive(n);
+        printPascMemoization(n);
 
 
     }
@@ -18,7 +25,26 @@ public class PascalsTriangle {
         return (a * fact(a-1));
     }
 
-    public static void printPascIterative(int n) {
+    //The common iterative way
+    public static void printPascIterative(int n){
+        for (int i = 0; i < n; i++) {
+            int value=1;
+            //spaces
+            for (int j = 0; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            //nums printing iterative
+            for (int j = 0; j <= i; j++) {
+                System.out.print(value+ " "); // Format each number with spacing
+                value = value * (i - j) / (j + 1); // Calculate next number
+            }
+            System.out.println();
+        }
+    }
+
+    //my way of doing things
+    public static void printPasc(int n) {
         //rows
         for (int i = 0; i < n; i++) {
 
@@ -27,13 +53,15 @@ public class PascalsTriangle {
                 System.out.print(" ");
             }
 
-            //nums printing iterative
+            //nums
             for (int j = 0; j <= i; j++) {
                 System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
             }
             System.out.println();
         }
     }
+
+    //Using recursion to solve the problem
     public static void printPascRecursive(int n) {
         //rows
         for (int i = 0; i < n; i++) {
@@ -43,14 +71,23 @@ public class PascalsTriangle {
                 System.out.print(" ");
             }
 
-            //nums printing iterative
+            //nums printing
             for (int j = 0; j <= i; j++) {
-                System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
+                System.out.print(pascalCalc(i, j) + " ");
             }
             System.out.println();
         }
     }
 
+    //calculating the pascal values for the triangle pattern
+    public static int pascalCalc(int i, int j){
+        if (j == 0 || j == i) {
+            return 1;
+        }
+            return pascalCalc(i - 1, j - 1) + pascalCalc(i - 1, j);
+    }
+
+    //Using memoization to optimize for efficiency
     public static void printPascMemoization(int n) {
         //rows
         for (int i = 0; i < n; i++) {
@@ -60,12 +97,27 @@ public class PascalsTriangle {
                 System.out.print(" ");
             }
 
-            //nums printing iterative
+            //nums
             for (int j = 0; j <= i; j++) {
-                System.out.print(fact(i) / (fact((i - j)) * fact(j)) + " ");
+                System.out.print(pascalCalcMemoization(i, j) + " ");
             }
             System.out.println();
         }
+    }
+    //calculating the pascal values for the triangle pattern with memoization
+    public static int pascalCalcMemoization(int i, int j){
+        if (j == 0 || j == i) {
+            return 1;
+        }
+
+        if (cache.containsKey(i+j)) {
+            return cache.get(i+j);
+        }
+
+        int result = pascalCalc(i - 1, j - 1) + pascalCalc(i - 1, j);
+        cache.put(i-j, result);
+
+        return result;
     }
 
 
